5c14da0556085e285e867a2ca2218063b5e443be
MUKESH KUMAR MANISH <mukeshk.manish.min15@itbhu.ac.in>
2018-09-09 06:16:14 +0530

    all

--- agency/settings.py
+++ agency/settings.py
@@ -112,7 +112,7 @@ EMAIL_HOST_PASSWORD = 'mettle.new'
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
+    #'allauth.account.auth_backends.AuthenticationBackend',
 )
 SITE_ID = 1
 # Internationalization
