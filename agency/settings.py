255276e934d66189b2dfe4a3fcc9100d35c37d29
abhimin15 <abhishek.lock.97@gmail.com>
2018-09-21 14:50:32 +0530

    link

--- agency/settings.py
+++ agency/settings.py
@@ -80,14 +80,14 @@ WSGI_APPLICATION = 'agency.wsgi.application'
 
 DATABASES = {
     'default': {
-		#'ENGINE':'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-		'ENGINE':'django.db.backends.mysql',
-        'NAME':'minovation2018',
-        'USER':'root',
-        'PASSWORD':'minovation18',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
+		'ENGINE':'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		#'ENGINE':'django.db.backends.mysql',
+        #'NAME':'minovation2018',
+        #'USER':'root',
+        #'PASSWORD':'minovation18',
+        #'HOST':'127.0.0.1',
+        #'PORT':'3306',
 		
     }
 }
